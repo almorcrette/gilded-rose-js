5a0690ea62830710a8db719329323c51fa383b2d
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-05-25 08:08:48 +0100

    refactor: switch old updateQuality for new and remove superfluous tests

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -11,64 +11,6 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      // Standard items (not Aged Brie, Backstage Passes or Sulfuras) with positive non-zero quality fall in quality by 1
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        // Non-standard items increase in quality by 1 (unless already with quality of 50)
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          // Backstage passes within 10 days of the concert increase by 2 unless already with quality 50
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            // Backstage passes within 5 days of concert increase by 2 unless already with quality 50
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      // All items sell by dates go down by 1 except Sulfuras
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      // All items past their sell-by dates fall in quality (unless already 0), except non-standard items (Aged brie, backstage passes and Sulfuras)
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          // Backstage passes past sell-by dates quality goes to 0
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-          // Brie quality continues to rise past sell-by date by a further 1 quality
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
-
-    return this.items;
-  }
-
-  newUpdateQuality() {
     this.items.forEach( item => {
       if (item.name === "Aged Brie") {
         this.updateAgedBrie(item)
